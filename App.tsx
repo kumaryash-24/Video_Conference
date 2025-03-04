0e38d105cada28cfb5893e9f160b5b010d26cfd5
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-03-04 11:16:24 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/Home';
+import Meeting from './components/Meeting';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/meeting/:id" element={<Meeting />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
