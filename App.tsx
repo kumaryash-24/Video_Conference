f35a58857b44dbcbf0d449d718fe5bc3f97808d3
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-03-28 20:37:07 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';   
 import Home from './components/Home';
 import Meeting from './components/Meeting';          
-    
+       
 function App() {     
   return (  
     <Router>
